e5e027245e7220d600c3c9af65169a4b467c7dd9
kalpelT <dgaming147@gmail.com>
2024-05-21 15:19:39 +0300

    sum-of-the-reverse-list

---
+++ sum-of-the-reverse-list/main.py
@@ -0,0 +1,10 @@
+my_list = [1,2,3,4,5,6,7,8]
+reversed_my_list = my_list[::-1]
+
+def sum_of_reversed_list(reversed_my_list):
+    return sum(reversed_my_list)
+
+print(reversed_my_list)
+print("Sum of the reversed list:",sum_of_reversed_list(reversed_my_list))
+
+
